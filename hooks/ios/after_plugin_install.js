a44d7cf9dc30c983b141883903a88255e1d96280
Xena Cheng （成啸） <Xena_X_Cheng@manulife.com>
2023-03-24 18:28:51 +0800

    DPI-16785 Add Distributed Tracing for iOS

--- hooks/ios/after_plugin_install.js
+++ hooks/ios/after_plugin_install.js
@@ -1,8 +1,13 @@
+/*
+ * Copyright (c) 2022-present New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 
+ */
+
 var ios = require('./ios');
 
 module.exports = function (context) {
   ios.verifyPlatformInstall(context);
-  if(ios.isPlatformConfigured()) {
+  if (ios.isPlatformConfigured()) {
     ios.removePostBuildScript(context);
     ios.injectPostBuildScript(context);
   }
