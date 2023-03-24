19aa85565b206632344322300a0e41d19fdee7a8
Xena Cheng （成啸） <Xena_X_Cheng@manulife.com>
2023-03-24 16:24:14 +0800

    2023R04 Add Distributed Tracing for iOS

--- src/ios/NewRelicCordovaPlugin.h
+++ src/ios/NewRelicCordovaPlugin.h
@@ -1,18 +1,58 @@
-//  New Relic for Mobile -- iOS edition
-//
-//  See:
-//    https://docs.newrelic.com/docs/mobile-apps for information
-//    https://docs.newrelic.com/docs/releases/ios for release notes
-//
-//  Copyright (c) 2014 New Relic. All rights reserved.
-//  See https://docs.newrelic.com/docs/licenses/ios-agent-licenses for license details
-//
+/*
+ * Copyright (c) 2022-present New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 
+ */
 
 #import <Foundation/Foundation.h>
-#import <NewRelicAgent/NewRelic.h>
+#import <NewRelic/NewRelic.h>
 #import <Cordova/CDV.h>
 
 @interface NewRelicCordovaPlugin : CDVPlugin {
     
 }
+
+- (void)recordBreadCrumb:(CDVInvokedUrlCommand *)command;
+
+- (void)recordCustomEvent:(CDVInvokedUrlCommand *)command;
+
+- (void)setAttribute:(CDVInvokedUrlCommand *)command;
+
+- (void)removeAttribute:(CDVInvokedUrlCommand *)command;
+
+- (void)setUserId:(CDVInvokedUrlCommand *)command;
+
+- (void)startInteraction:(CDVInvokedUrlCommand *)command;
+
+- (void)endInteraction:(CDVInvokedUrlCommand *)command;
+
+- (void)noticeDistributedTrace:(CDVInvokedUrlCommand *)command;
+
+- (void)noticeHttpTransaction:(CDVInvokedUrlCommand *)command;
+
+- (void)recordError:(CDVInvokedUrlCommand *)command;
+
+- (void)crashNow:(CDVInvokedUrlCommand *)command;
+
+- (void)currentSessionId:(CDVInvokedUrlCommand *)command;
+
+- (void)incrementAttribute:(CDVInvokedUrlCommand *)command;
+
+- (void)noticeNetworkFailure:(CDVInvokedUrlCommand *)command;
+
+- (void)recordMetric:(CDVInvokedUrlCommand *)command;
+
+- (void)removeAllAttributes:(CDVInvokedUrlCommand *)command;
+
+- (void)setMaxEventBufferTime:(CDVInvokedUrlCommand *)command;
+
+- (void)setMaxEventPoolSize:(CDVInvokedUrlCommand *)command;
+
+- (void)analyticsEventEnabled:(CDVInvokedUrlCommand *) command;
+
+- (void)networkRequestEnabled:(CDVInvokedUrlCommand *) command;
+
+- (void)networkErrorRequestEnabled:(CDVInvokedUrlCommand *) command;
+
+- (void)httpRequestBodyCaptureEnabled:(CDVInvokedUrlCommand *) command;
+
 @end
